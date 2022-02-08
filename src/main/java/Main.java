a5c357f35a839187196aba2da400e495abf1c9ec
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-08 14:46:41 +0900

    Implement StringSortService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import logic.BubbleSort;
+import logic.JavaSort;
+import service.StringSortService;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        StringSortService service = new StringSortService(new BubbleSort<>());
+        List<String> list = service.doSort(Arrays.asList(args));
+
+        System.out.println("Hello World !- result: " + list.toString());
     }
 }
--- src/main/java/logic/JavaSort.java
+++ src/main/java/logic/JavaSort.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort <T extends Comparable<T>> {
+public class JavaSort <T extends Comparable<T>> implements Sort<T> {
 
-    public List<T> sort(List<T> list) {
+    @Override
+    public List sort(List<T> list) {
         List<T> output = new ArrayList<>(list);
         Collections.sort(output);
 
